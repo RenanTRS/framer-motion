374268bd7604bb188159220db8f353644cda5573
Renan Souza <renantrsouza@gmail.com>
2022-09-08 12:38:21 -0400

    add: animations

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,35 @@
 import { Link } from 'react-router-dom';
 import style from './Home.module.scss'
+import {motion} from 'framer-motion'
 
 export const Home = () => {
   return (
-    <div className={`${style.home} container`}>
-      <h2>
+    <motion.div className={`${style.home} container`}
+      animate={{
+        rotateZ: 180,
+        opacity: 0.2,
+        marginTop: 200
+      }}
+    >
+      <motion.h2
+        animate={{
+          fontSize: '50px',
+          color: '#ff2994',
+          x: 100,
+          y: -100
+        }}
+      >
         Welcome to Pizza Joint
-      </h2>
+      </motion.h2>
       <Link to="/base">
-        <button>
+        <motion.button
+          animate={{
+            scale: 1.5
+          }}
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
